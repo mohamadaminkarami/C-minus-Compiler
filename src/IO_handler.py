db99099902d688f1bf425652be3efc5a2f562967
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2023-01-05 14:07:16 +0330

    Fix IOHandler for quera judge

--- src/IO_handler.py
+++ src/IO_handler.py
@@ -9,8 +9,8 @@ class IOHandler:
     def read_input(self, num):
         self.num = num
 
-        file = open(f"./p2_tests/T{num}/input.txt", "r")
-        # file = open(f"input.txt", "r") # for quera judge
+        # file = open(f"./p2_tests/T{num}/input.txt", "r")  # for testing
+        file = open(f"input.txt", "r")  # for quera judge
 
         return file.read()
 
@@ -22,8 +22,8 @@ class IOHandler:
         return s
 
     def write_lexical_errors(self, lexical_errors: Dict[int, List[Tuple[str, str]]]):
-        file = open(f"./p2_res/T{self.num}/lexical_errors.txt", "w+")
-        # file = open(f"lexical_errors.txt", "w+")  # for quera judge
+        # file = open(f"./p2_res/T{self.num}/lexical_errors.txt", "w+")  # for testing
+        file = open(f"lexical_errors.txt", "w+")  # for quera judge
 
         if len(lexical_errors.keys()) == 0:
             file.write("There is no lexical error.")
@@ -34,8 +34,8 @@ class IOHandler:
         file.close()
 
     def write_tokens(self, tokens: Dict[int, List[Tuple[TokenType, str]]]):
-        file = open(f"./p2_res/T{self.num}/tokens.txt", "w+")
-        # file = open(f"tokens.txt", "w+") # for quera judge
+        # file = open(f"./p2_res/T{self.num}/tokens.txt", "w+")  # for testing
+        file = open(f"tokens.txt", "w+")  # for quera judge
 
         for line_number, token_list in tokens.items():
             file.write(f"{line_number}.\t{self._list_to_str(token_list)}\n")
@@ -43,26 +43,26 @@ class IOHandler:
         file.close()
 
     def write_symbol_tables(self, symbols: List[SymbolTableRow]):
-        file = open(f"./p2_res/T{self.num}/symbol_table.txt", "w+")
-        # file = open(f"symbol_table.txt", "w+") for quera judge
+        # file = open(f"./p2_res/T{self.num}/symbol_table.txt", "w+")  # for testing
+        file = open(f"symbol_table.txt", "w+")  # for quera judge
 
         for index, symbol_table_row in enumerate(symbols):
             file.write(f"{index + 1}.\t{symbol_table_row}\n")
         file.close()
 
     def write_parse_tree(self, parent: Node):
-        tree_str = ''
+        tree_str = ""
         if parent:
             for pre, fill, node in RenderTree(parent):
-                tree_str += "%s%s" % (pre, node.name) + '\n'
-        f = open(f"./p2_res/T{self.num}/parse_tree.txt", "w", encoding='utf-8')
-        # file = open(f"parse_tree.txt", "w+")  # for quera judge
-        f.write(tree_str.strip())
-        f.close()
+                tree_str += "%s%s" % (pre, node.name) + "\n"
+        # file = open(f"./p2_res/T{self.num}/parse_tree.txt", "w")  # for testing
+        file = open(f"parse_tree.txt", "w+")  # for quera judge
+        file.write(tree_str.strip())
+        file.close()
 
     def write_syntax_errors(self, syntax_errors: List[str]):
-        file = open(f"./p2_res/T{self.num}/syntax_errors.txt", "w+")
-        # file = open(f"syntax_errors.txt", "w+")  # for quera judge
+        # file = open(f"./p2_res/T{self.num}/syntax_errors.txt", "w+")  # for testing
+        file = open(f"syntax_errors.txt", "w+")  # for quera judge
 
         if syntax_errors:
             for message in syntax_errors:
